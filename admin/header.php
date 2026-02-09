7907b1b3c4b74398ddef9e7107b129234fafccb2
Akbar-fajar90 <xirpl611@gmail.com>
2026-02-09 07:57:10 +0700

    Menambahkan file cetak penjualan dan cetak laporan

--- admin/header.php
+++ admin/header.php
@@ -4,7 +4,7 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-   <title>Penjualan App</title>
+   <center><title>Penjualan App</title></center>
 </head>
 <body style="background: #f3f3f3ff">
 <?php
---
+++ admin/laporan_cetak.php
@@ -0,0 +1,77 @@
+<?php
+include("../koneksi.php");
+include("header.php");
+?>
+
+<body>
+
+    <center>
+        <h2>LAPORAN PENJUALAN</h2>
+        <p>Dicetak oleh: <?php echo $_SESSION['ussername']; ?> pada <?php echo date('d-m-Y H:i:s'); ?></p>
+    </center>
+
+    <br/>
+
+    <table class="table table-bordered table-striped">
+        <thead>
+            <tr>
+                <th width="1%">NO</th>
+                <th>Invoice</th>
+                <th>Kasir</th>
+                <th>Barang</th>
+                <th>Tgl. Transaksi</th>
+                <th>Total Harga</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php 
+            $id_user = $_SESSION["user_id"];
+            
+            $data = mysqli_query($koneksi, "
+                SELECT penjualan.*, barang.nama_barang, user.user_nama 
+                FROM penjualan 
+                JOIN barang ON penjualan.id_barang = barang.id_barang 
+                JOIN user ON penjualan.user_id = user.user_id
+                WHERE penjualan.user_id = '$id_user' 
+                ORDER BY penjualan.id_jual DESC
+            ") or die(mysqli_error($koneksi));
+            
+            $no = 1;
+            $grand_total = 0;
+            while ($d = mysqli_fetch_array($data)) {
+                $grand_total += $d['total_harga'];
+            ?>
+            <tr>
+                <td class="text-center"><?php echo $no++; ?></td>
+                <td><?php echo "INV-".$d['id_jual']; ?></td>
+                <td><?php echo $d['user_nama']; ?></td>
+                <td><?php echo $d['nama_barang']; ?></td>
+                <td><?php echo date('d-m-Y', strtotime($d['tgl_jual'])); ?></td>
+                <td class="text-right">Rp <?php echo number_format($d['total_harga']); ?></td>
+            </tr>
+            <?php } ?>
+        </tbody>
+        <tfoot>
+            <tr>
+                <th colspan="5" class="text-right">TOTAL PENDAPATAN SAYA</th>
+                <th class="text-right">Rp <?php echo number_format($grand_total); ?></th>
+            </tr>
+        </tfoot>
+    </table>
+
+    <br/>
+    
+    <div class="row">
+        <div class="col-xs-8"></div>
+        <div class="col-xs-4 text-center">
+            <p>Tertanda,</p>
+            <br/><br/><br/>
+            <p><b><?php echo $_SESSION['ussername']; ?></b></p>
+        </div>
+    </div>
+
+    <script>
+        window.print();
+    </script>
+
+</body>
